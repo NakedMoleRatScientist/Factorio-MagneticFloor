8929e399f1c4b68cf36748c2be47c034fdb1809b
Kiba <hackerkiba@gmail.com>
2016-02-04 01:13:19 -0500

    disallow anything more than one hoverboard in the equipment grid

--- control.lua
+++ control.lua
@@ -3,18 +3,25 @@ require 'defines'
 
 script.on_event(defines.events.on_tick, function(event)
   tile = game.player.surface.get_tile(game.player.position["x"],game.player.position["y"])
-  if tile.name ~= "copper-floor" then
-    --http://www.factorioforums.com/forum/viewtopic.php?f=25&t=16571
-    armor = game.player.get_inventory(defines.inventory.player_armor)[1]
-    if armor.valid_for_read then
-      if armor.has_grid then
-        equipment = armor.grid.equipment
-        for i,e in pairs(equipment) do
-          if e.name == "hoverboard" then
+  local count =  0
+  --http://www.factorioforums.com/forum/viewtopic.php?f=25&t=16571
+  armor = game.player.get_inventory(defines.inventory.player_armor)[1]
+  if armor.valid_for_read then
+    if armor.has_grid then
+      equipment = armor.grid.equipment
+      for i,e in pairs(equipment) do
+        if e.name =="hoverboard" then
+          if tile.name ~= "copper-floor" then
             e.energy = 0
           end
+          if count > 0 then
+            game.player.insert(armor.grid.take(e))
+          end
+          count = count + 1
         end
+        game.player.print(count)
       end
     end
   end
+
 end)
