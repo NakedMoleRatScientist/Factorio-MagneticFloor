e3795eedc9759f761616a77075af772dfe13c992
Kiba <hackerkiba@gmail.com>
2016-02-06 00:30:54 -0500

    update icon

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -2,7 +2,7 @@ data:extend({
   {
     type = "technology",
     name = "copper-floor",
-    icon = "__base__/graphics/icons/copper-plate.png",
+    icon = "__MagneticFloor__/graphics/icons/copper-floor-icon.png",
     prerequisites =
     {
       "concrete",
@@ -51,6 +51,62 @@ data:extend({
       {
         {"science-pack-1",1},
         {"science-pack-2",1}
+      }
+    },
+    order = "[h]"
+  },
+  {
+    type = "technology",
+    name = "copper-floor2",
+    icon = "__MagneticFloor__/graphics/icons/copper-floor-icon_level2.png",
+    prerequisites =
+    {
+      "copper-floor",
+    },
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "copper-floor2",
+
+      }
+    },
+    unit =
+    {
+      count = 100,
+      time = 35,
+      ingredients =
+      {
+        {"science-pack-1",2},
+        {"science-pack-2",2}
+      }
+    },
+    order = "[h]"
+  },
+  {
+    type = "technology",
+    name = "copper-floor3",
+    icon = "__MagneticFloor__/graphics/icons/copper-floor-icon_level3.png",
+    prerequisites =
+    {
+      "copper-floor2",
+    },
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "copper-floor3",
+
+      }
+    },
+    unit =
+    {
+      count = 150,
+      time = 60,
+      ingredients =
+      {
+        {"science-pack-1",3},
+        {"science-pack-2",3}
       }
     },
     order = "[h]"
