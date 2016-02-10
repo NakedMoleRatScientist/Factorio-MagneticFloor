2b3a5db37e835e82f9144ecd1d4c40df92bf6cb3
Kiba <hackerkiba@gmail.com>
2016-02-10 04:30:23 -0500

    rewrite make.sh

--- make.sh
+++ make.sh
@@ -1,1 +1,4 @@
- zip -r MagneticFloor_0.0.4.zip ./* -x *.xcf* *.zip* *.DS_Store*
+project="MagneticFloor_0.0.4"
+mkdir $project
+cp -R info.json CHANGES LICENSE README data.lua control.lua locales prototypes graphics migrations $project/
+zip -r MagneticFloor_0.0.4.zip $project -x *.xcf* *.zip* *.DS_Store*
