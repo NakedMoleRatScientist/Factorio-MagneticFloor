30970b4ec12fc0812af0962fc58dc509440fd627
Kiba <hackerkiba@gmail.com>
2016-06-20 11:25:48 -0400

    get rid of unnecessary code that doesn't do much of anything

--- control.lua
+++ control.lua
@@ -55,29 +55,6 @@ function activeHoverMode()
   end
 end
 
-function limitHoverboard()
-  local tile = getTile()
-  local count =  0
-  --http://www.factorioforums.com/forum/viewtopic.php?f=25&t=16571
-  local armor = game.player.get_inventory(defines.inventory.player_armor)[1]
-  if armor.valid_for_read then
-    if armor.has_grid then
-      local equipment = armor.grid.equipment
-      for i,e in pairs(equipment) do
-        if e.name =="hoverboard" then
-          if inboundTile(tile.name) == false then
-            e.energy = 0
-          end
-          if count > 0 then
-            game.player.insert(armor.grid.take(e))
-          end
-          count = count + 1
-        end
-      end
-    end
-  end
-end
-
 function initializeGUI()
   if game.player.gui.top.hoverboard ~= nil then
     game.player.gui.top.hoverboard.destroy()
@@ -101,7 +78,6 @@ function updateStatusGUI()
 end
 
 function hoverMode()
-  limitHoverboard()
   if global.hoverboard.status == true then
     activeHoverMode()
     tileCheck()
