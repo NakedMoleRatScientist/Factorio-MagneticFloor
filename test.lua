ea3781ec2aaa40fb198776c22826a4232e6e3033
Kiba <hackerkiba@gmail.com>
2016-07-03 02:52:19 -0400

    add remote interface

--- test.lua
+++ test.lua
@@ -1,9 +1,12 @@
-function getItems()
-  game.players[1].insert_item{name = "hoverboard", count = 1}
-  game.players[1].insert_item{name = "copper-floor3", count = 200}
-  game.players[1].insert_item{name = "accelerator", count = 20}
-  game.players[1].insert_item{name = "left", count = 5}
-  game.players[1].insert_item{name = "right", count = 5}
-  game.players[1].insert_item{name = "down", count = 5}
-  game.players[1].insert_item{name = "up", count = 5}
-end
+remote.add_interface("mag",
+{
+  getItems = function()
+    game.players[1].insert_item{name = "hoverboard", count = 1}
+    game.players[1].insert_item{name = "copper-floor3", count = 200}
+    game.players[1].insert_item{name = "accelerator", count = 20}
+    game.players[1].insert_item{name = "left", count = 5}
+    game.players[1].insert_item{name = "right", count = 5}
+    game.players[1].insert_item{name = "down", count = 5}
+    game.players[1].insert_item{name = "up", count = 5}
+  end
+})
