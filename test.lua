4d8bfda0bd639750c7845922c9089ca78b5d7d50
Kiba <hackerkiba@gmail.com>
2016-07-03 02:46:12 -0400

    add handy functions for testing stuff

---
+++ test.lua
@@ -0,0 +1,9 @@
+function getItems()
+  game.players[1].insert_item{name = "hoverboard", count = 1}
+  game.players[1].insert_item{name = "copper-floor3", count = 200}
+  game.players[1].insert_item{name = "accelerator", count = 20}
+  game.players[1].insert_item{name = "left", count = 5}
+  game.players[1].insert_item{name = "right", count = 5}
+  game.players[1].insert_item{name = "down", count = 5}
+  game.players[1].insert_item{name = "up", count = 5}
+end
