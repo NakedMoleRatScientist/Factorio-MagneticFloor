1132874a5c3d896d580ddc189600ed301f138070
Kiba <hackerkiba@gmail.com>
2016-07-03 22:07:51 -0400

    not insert_item, just insert

--- test.lua
+++ test.lua
@@ -1,12 +1,12 @@
 remote.add_interface("mag",
 {
   getItems = function()
-    game.players[1].insert_item{name = "hoverboard", count = 1}
-    game.players[1].insert_item{name = "copper-floor3", count = 200}
-    game.players[1].insert_item{name = "accelerator", count = 20}
-    game.players[1].insert_item{name = "left", count = 5}
-    game.players[1].insert_item{name = "right", count = 5}
-    game.players[1].insert_item{name = "down", count = 5}
-    game.players[1].insert_item{name = "up", count = 5}
+    game.players[1].insert{name = "hoverboard", count = 1}
+    game.players[1].insert{name = "copper-floor3", count = 200}
+    game.players[1].insert{name = "accelerator", count = 20}
+    game.players[1].insert{name = "left", count = 5}
+    game.players[1].insert{name = "right", count = 5}
+    game.players[1].insert{name = "down", count = 5}
+    game.players[1].insert{name = "up", count = 5}
   end
 })
