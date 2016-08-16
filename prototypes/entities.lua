1f7d22443150765cde2595d33f90c2018a884e35
Kiba <hackerkiba@gmail.com>
2016-08-16 18:30:31 -0400

    promptly delete all pictures and replace with custom graphic

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -69,52 +69,9 @@ data:extend({
     pictures =
     {
       {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-01.png",
-        width = 201,
-        height= 242,
-        shift = {1.9, -0.68}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-02.png",
-        width = 307,
-        height= 256,
-        shift = {2.62, -0.68}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-03.png",
-        width = 286,
-        height= 240,
-        shift = {0.56, -1.5}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-04.png",
-        width = 291,
-        height= 229,
-        shift = {3.5, -2.0}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-05.png",
-        width = 265,
-        height= 264,
-        shift = {3.2, -0.46}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-06.png",
-        width = 312,
-        height= 267,
-        shift = {2.59, -1.34}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-07.png",
-        width = 294,
-        height= 213,
-        shift = {3.37, -0.25}
-      },
-      {
-        filename = "__base__/graphics/entity/tree/dry-hairy-tree/dry-hairy-tree-08.png",
-        width = 284,
-        height= 217,
-        shift = {2.28, -1.25}
+        filename = "__MagneticFloor__/graphics/directives/up.png",
+        width = 32,
+        height= 32,
       }
     }
   }
