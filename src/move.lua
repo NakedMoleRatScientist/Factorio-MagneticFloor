6f57769e039532180a69fac3ea6c434e0e694322
Kiba <hackerkiba@gmail.com>
2016-08-18 16:48:31 -0400

    getEntity

--- src/move.lua
+++ src/move.lua
@@ -2,9 +2,18 @@ function getTile(index)
   return game.players[index].surface.get_tile(game.players[index].position.x,game.players[index].position.y)
 end
 
+function getEntity(index)
+  position = {game.players[index].position.x, game.players[index].position.y}
+  return game.players[index].surface.find_entity("up",position)
+end
+
 function tileCheck(index)
   local tile = getTile(index)
   local walk = game.players[index].walking_state.walking
+  local entity = getEntity(index)
+  if entity ~= nil then
+    print("YES!")
+  end
   if tile.name == "accelerator" then
     charge_hoverboard(index,tile)
   elseif tile.name == "down" and global.hoverboard[index].charge > 0 then
