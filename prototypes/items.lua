b66d869eb70bf22bf1a24245f9a3bd7935b76af8
Kiba <hackerkiba@gmail.com>
2016-08-19 08:26:08 -0400

    place charger instead

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -54,12 +54,7 @@ data:extend({
     name = 'accelerator',
     icon = "__MagneticFloor__/graphics/directives/accelerator.png",
     flags = {"goes-to-quickbar"},
-    place_as_tile =
-    {
-      result = "accelerator",
-      condition_size = 1,
-      condition = { "water-tile" }
-    },
+    place_result = "accelerator_charger"
     stack_size = 50,
     order = "b[concrete]"
   },
