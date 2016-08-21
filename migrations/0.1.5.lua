a8eca124ac9e995a9bc1a1d4f16054c2fc1c089d
Kiba <hackerkiba@gmail.com>
2016-08-21 19:25:43 -0400

    add a function for finding certain entities on a position

--- migrations/0.1.5.lua
+++ migrations/0.1.5.lua
@@ -1,3 +1,14 @@
 
 
-entities = {"accelerator_charger","up","right","down","left"}
+
+function findDirectiveEntities(surface,position)
+  local entities = {"accelerator_charger","up","right","down","left"}
+  local list = {"left","down","up","right","accelerator_charger"}
+  for _, name in ipairs(entities) do
+    local target = surface.find_entity(name,position)
+    if target ~= nil then
+      return target
+    end
+  end
+  return nil
+end
