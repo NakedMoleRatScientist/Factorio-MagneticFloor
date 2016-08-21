fdf7a5c4c00ce9e80d5139d37ad5b514b073a1a6
Kiba <hackerkiba@gmail.com>
2016-08-21 19:30:11 -0400

    get rid of a duplicate table

--- migrations/0.1.5.lua
+++ migrations/0.1.5.lua
@@ -3,7 +3,6 @@
 
 function findDirectiveEntities(surface,position)
   local entities = {"accelerator_charger","up","right","down","left"}
-  local list = {"left","down","up","right","accelerator_charger"}
   for _, name in ipairs(entities) do
     local target = surface.find_entity(name,position)
     if target ~= nil then
