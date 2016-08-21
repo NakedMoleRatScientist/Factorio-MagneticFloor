6140a423c2ec5c2832e9356c3be4fee023592c8c
Kiba <hackerkiba@gmail.com>
2016-08-21 19:48:22 -0400

    add code snippet from the foru, and add a new function called matchTile

--- migrations/0.1.5.lua
+++ migrations/0.1.5.lua
@@ -12,4 +12,27 @@ function findDirectiveEntities(surface,position)
   return nil
 end
 
-old_tiles = {"accelerator","up","right","down"}
+function matchTile(name)
+  old_tiles = {"accelerator","up","right","down"}
+  for _, t in ipairs(old_tiles) do
+    if t == name then
+      return true
+    end
+  end
+end
+
+
+
+--https://forums.factorio.com/viewtopic.php?p=195459#p195459
+for _, surface in pairs(game.surfaces) do
+    for chunk in surface.get_chunks() do
+        for x = 0, 31 do
+            for y = 0, 31 do
+                local pos = {chunk.x * 32 + x, chunk.y * 32 + y}
+                if surface.get_tile(pos[1], pos[2]).name == "dirt" then
+                    surface.create_entity{name="inserter", position=pos} --might also want to specify a force here
+                end
+            end
+        end
+    end
+end
