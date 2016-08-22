ac882ceb45e87a8f7d76a55a19275a46f98efe30
Kiba <hackerkiba@gmail.com>
2016-08-21 20:26:07 -0400

    add an AND condition to check if the tile is an accelerator

--- migrations/0.1.5.lua
+++ migrations/0.1.5.lua
@@ -21,7 +21,7 @@ for _, surface in pairs(game.surfaces) do
                 change_tiles.insert({name="copper-floor3",pos})
                 local accel = surface.find_entity("accelerator",pos)
                 -- In 0.1.4, the accelerator_charger entity was created on top of the accelerator tile. in 0.1.5, the entities are created for all the other tiles. Hence, we only need to find if accelerator exists. If not, create them.
-                if accel == nil then
+                if accel == nil and tile.name == "accelerator" then
                   surface.create_entity({name="accelerator_charger",position=pos,force="player"})
                 else
                   surface.create_entity({name=tile.name,position=pos,force="player"})
