27756f65502103688a3f587d1c8341b7d3a06f3a
Kiba <hackerkiba@gmail.com>
2016-08-21 20:07:42 -0400

    reindent a region

--- migrations/0.1.5.lua
+++ migrations/0.1.5.lua
@@ -11,16 +11,16 @@ end
 change_tiles = {}
 --https://forums.factorio.com/viewtopic.php?p=195459#p195459
 for _, surface in pairs(game.surfaces) do
-    for chunk in surface.get_chunks() do
-        for x = 0, 31 do
-            for y = 0, 31 do
-                local pos = {chunk.x * 32 + x, chunk.y * 32 + y}
-                local tile = surface.get_tile(pos[1],pos[2])
-                if matchTile(tile.name) then
-                  change_tiles.insert(tile)
---                    surface.create_entity{name="inserter", position=pos} --might also want to specify a force here
-                end
-            end
-        end
-    end
+  for chunk in surface.get_chunks() do
+      for x = 0, 31 do
+          for y = 0, 31 do
+              local pos = {chunk.x * 32 + x, chunk.y * 32 + y}
+              local tile = surface.get_tile(pos[1],pos[2])
+              if matchTile(tile.name) then
+                change_tiles.insert(tile)
+--              surface.create_entity{name="inserter", position=pos} --might also want to specify a force here
+              end
+          end
+      end
+  end
 end
