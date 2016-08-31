aed97d24433c87ae8f25f45bedb6e7c7672b1823
Kiba <hackerkiba@gmail.com>
2016-08-31 18:38:44 -0400

    fix and cleanup

--- src/armor.lua
+++ src/armor.lua
@@ -1,20 +1,15 @@
 function getArmor(index)
   local armor = game.players[index].get_inventory(defines.inventory.player_armor)[1]
-  return armor
-end
-
-function armorCheck(index)
-  local armor = getArmor(index)
-  if armor.grid ~= nil then
-    return true
+  if armor ~= nil and armor.valid_for_read == true then
+    return armor
   end
   return false
 end
 
 script.on_event(defines.events.on_player_armor_inventory_changed, function(event)
   local index = event.player_index
-  if armorCheck(index) then
-    local armor = getArmor(index)
+  local armor = getArmor(index)
+  if armor ~= false then
     local equipment = armor.grid.equipment
     for i, e in ipairs(equipment) do
       if e.name == "hoverboard" then
