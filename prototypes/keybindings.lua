1a3bf09ee006a44fee22374e3e87f002fa182e52
Kiba <hackerkiba@gmail.com>
2016-09-05 20:43:23 -0400

    use enter not return

--- prototypes/keybindings.lua
+++ prototypes/keybindings.lua
@@ -2,7 +2,7 @@ data:extend({
   {
     type = "custom-input",
     name = "switch_hoverboard_mode",
-    key_sequence = "RETURN",
+    key_sequence = "ENTER",
     consuming = "script-only"
   }
 })
